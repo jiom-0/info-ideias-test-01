36484f57c3af4f8a9e97b1953c8b6fe8230b8e01
manjaro <73485215+jiom-0@users.noreply.github.com>
2021-08-24 19:27:45 -0300

    0.02

--- primoinferior.php
+++ primoinferior.php
@@ -1,3 +1,32 @@
+<html>
+<body>
+<h1>
 <?php
+	/*2 -Função PrimoInferior($num)
+	Crie uma função que receba como parâmetro um número inteiro e retorne o maior número primo inferior ao número recebido como parâmetro.
+	Se o argumento for negativo, a função deverá retornar o valor zero.
+	*/
+	
+	function PrimoInferior($primo){
+		$resp=false;
+		for($x=$primo-1;$x>1;$x--){
+			$resp=$x;
+			for($num=$x-1;$num>1;$num--){
+				if($x%$num==0){
+					$resp=false;
+					break;
+				}
+			}
+			if($resp==$x){
+				return $resp;			
+			}
+		}
+	}
 
+	echo "Número 8 = ".PrimoInferior(8)."</br>";
+	echo "Número 10 = ".PrimoInferior(10)."</br>";
+	echo "Número 30 = ".PrimoInferior(30)."</br>";
 ?>
+</h1>
+</body>
+</html>
