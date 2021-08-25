868699e74d96f790b8b7c37338b9ede17c55dbe3
jiom-0 <73485215+jiom-0@users.noreply.github.com>
2021-08-24 23:39:56 -0300

    Update primoinferior.php

--- primoinferior.php
+++ primoinferior.php
@@ -6,10 +6,10 @@
 	Crie uma função que receba como parâmetro um número inteiro e retorne o maior número primo inferior ao número recebido como parâmetro.
 	Se o argumento for negativo, a função deverá retornar o valor zero.
 	*/
-	
+
 	function PrimoInferior($num){
 		$resp=false;
-		for($x=$primo-1;$x>1;$x--){
+		for($x=$num-1;$x>1;$x--){
 			$resp=$x;
 			for($a=$x-1;$a>1;$a--){
 				if($x%$a==0){
@@ -29,4 +29,4 @@
 ?>
 </h1>
 </body>
-</html>
+</html> 
